5ec2c95f63a296f5e7059744c3cf0f4af3173359
Saniya_Shete <shetesaniya22@gmail.com>
2026-03-16 17:03:32 +0530

    import react

--- src/components/App.js
+++ src/components/App.js
@@ -1,17 +1,20 @@
-
-import { useState } from 'react';
-import './../styles/App.css';
-
+import React, { useState } from "react";
+import "./../styles/App.css";
 
 const App = () => {
- const [count, setCount] = useState(0);
+
+  const [count, setCount] = useState(0);
+
+  const handleClick = () => {
+    setCount(count + 1);
+  };
+
   return (
     <div>
       <p>Button clicked {count} times</p>
       <button onClick={handleClick}>Click Me</button>
-        
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
